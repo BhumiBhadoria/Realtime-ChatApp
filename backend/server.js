8804bed2f1758139eff2774e7b6deb7664f6c082
BhumiBhadoria <113108322+BhumiBhadoria@users.noreply.github.com>
2023-08-11 11:02:12 +0530

    updations made

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { chats } = require('./data/data');
+const dotenv = require('dotenv');
 
 const app = express();
+dotenv.config();
 
 //API created
 app.get('/', (req, res) => {
@@ -13,9 +15,13 @@ app.get('/api/chat', (req,res) => {
 });
 
 app.get('/api/chat/:id', (req,res) => {
-    console.log(req);
+    // console.log(req.params.id);
+    const singlechat = chats.find((c) => c._id === req.params.id);
+    res.send(singlechat);
     
 });
 
+const PORT = process.env.PORT || 5000
+
 //new web server started
-app.listen(5000, console.log("Server started on PORT 5000"));   
+app.listen(${PORT}, console.log('Server started on PORT ${PORT}'));   
